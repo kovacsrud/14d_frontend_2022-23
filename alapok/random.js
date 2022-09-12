01ff7eca10b1b5044746d0a49d2289cdc9114b2d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-09-12 13:43:21 +0200

    Random

--- alapok/random.js
+++ alapok/random.js
@@ -3,4 +3,21 @@
 
 //Kiindulási alap: Math.random()
 
-Math.random()
+console.log(Math.floor(Math.random()*50)+10);
+
+function randomSzam(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+console.log(randomSzam(10,90));
+
+//Lista generálás és megjelenítés
+let vezetekNevek=["Kiss","Nagy","Boldog","Kovács","Szekeres","Kertész","Zsiga"];
+let keresztNevek=["Alex","Éva","Zsombor","Ubul","Zénó","Tünde","Iván"];
+//életkor 0-120
+let lakhely=["Gyula","Mezőhegyes","Orosháza","Nagyszénás","Ózd","Lenti"];
+
+//készítsen listát olyan objektumokból, amelyek ilyen személyek adatait
+//tartalmazzák
+let szemelyLista=[];
+//szemelyLista.push();
