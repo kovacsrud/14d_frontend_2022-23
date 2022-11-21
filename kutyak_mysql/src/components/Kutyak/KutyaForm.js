d293f9001bf3459ea838c06a90982cca03bab62e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-21 11:50:56 +0100

    From készítés

--- kutyak_mysql/src/components/Kutyak/KutyaForm.js
+++ kutyak_mysql/src/components/Kutyak/KutyaForm.js
@@ -1,9 +1,65 @@
-
+import { useState, useEffect, useContext } from "react";
+import KutyafajtaContext from '../context/KutyfajtaContext';
 
 function KutyaForm() {
+  const {kutyafajtak}=useContext(KutyafajtaContext);
+  const [kutyanevek,setKutyanevek]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:8000/api/rendelo/kutyanevek')
+    .then(res=>res.json())
+    .then(adat=>setKutyanevek(adat));
+  },[]);
+  
+
   return (
-    <div>KutyaForm</div>
-  )
+    <div className="flex justify-center items-center">
+      <div className="flex-column">
+        <h2>Új rendelési adat felvitele</h2>
+        <div className="form-control w-full max-w-xs my-2">
+          <label className="label">
+            <span className="label-text">A kutya életkora</span>
+          </label>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            id="eletkor"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs my-2">
+          <label className="label">
+            <span className="label-text">Utolsó ellenőrzés dátuma</span>
+          </label>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            id="utolsoell"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs my-2">
+          <label className="label">
+            <span className="label-text">Válasszon kutyanevet</span>           
+          </label>
+          <select className="select select-bordered" id="nev">
+           {
+              kutyanevek.map((kutyanev)=>(<option key={kutyanev.id}>{kutyanev.kutyanev}</option>))
+           } 
+          </select>         
+        </div>
+        <div className="form-control w-full max-w-xs my-2">
+          <label className="label">
+            <span className="label-text">Válasszon kutyafajtát!</span>           
+          </label>
+          <select className="select select-bordered" id="fajtanev">
+           {
+             kutyafajtak.map((kutyafajta)=>(<option key={kutyafajta.id}>{kutyafajta.nev}</option>))
+           }
+          </select>         
+        </div>
+        <button type="submit" className="btn btn-primary">Küldés</button>
+      </div>
+    </div>
+  );
 }
 
-export default KutyaForm
+export default KutyaForm;
--- kutyak_mysql/src/components/Kutyak/KutyaLista.js
+++ kutyak_mysql/src/components/Kutyak/KutyaLista.js
@@ -1,12 +1,22 @@
 import KutyaContext from "../context/KutyaContext";
 import {useContext} from "react";
 import Kutya from "./Kutya";
+import {useNavigate} from 'react-router-dom';
 
 function KutyaLista() {
+    const navigate=useNavigate();
+
+    const ujkutya=()=>{
+        navigate('/ujkutya');
+    }
+
     const {kutyak}=useContext(KutyaContext);
   return (
     <div className='flex justify-center items-center'>
+         
         <div className='flex-column'>
+        <button onClick={ujkutya} className="btn btn-primary">Új rendelési adat felvitele</button>   
+       
         {
             kutyak.map((kutya,index)=>(<Kutya key={kutya.id} kutya={kutya} />))
         }
