188af6e92aa3da469e00862745a740f444adc61b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-21 13:07:02 +0100

    Új adat felvitele

--- kutyak_mysql/src/components/Kutyak/KutyaForm.js
+++ kutyak_mysql/src/components/Kutyak/KutyaForm.js
@@ -1,9 +1,33 @@
 import { useState, useEffect, useContext } from "react";
 import KutyafajtaContext from '../context/KutyfajtaContext';
+import KutyaContext from "../context/KutyaContext";
+import {useNavigate} from 'react-router-dom';
 
 function KutyaForm() {
+  const navigate=useNavigate();
+  const {adatKuldes}=useContext(KutyaContext);
+  let cim="Új rendelési adat felvitele";
+  let method="POST";
+
+  let formObj={
+    nevid:1,
+    fajtaid:1,
+    eletkor:"",
+    utolsoell:""
+  }
   const {kutyafajtak}=useContext(KutyafajtaContext);
   const [kutyanevek,setKutyanevek]=useState([]);
+  const [formData,setFormData]=useState(formObj);
+
+  const writeData=(e)=>{
+    setFormData((prevState)=>({...prevState,[e.target.id]:e.target.value}));
+  }
+
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    adatKuldes(formData,method);
+    navigate('/kutyak');
+  }
 
   useEffect(()=>{
     fetch('http://localhost:8000/api/rendelo/kutyanevek')
@@ -15,13 +39,14 @@ function KutyaForm() {
   return (
     <div className="flex justify-center items-center">
       <div className="flex-column">
-        <h2>Új rendelési adat felvitele</h2>
+        <h2>{cim}</h2>
+        <form onSubmit={onSubmit}>
         <div className="form-control w-full max-w-xs my-2">
           <label className="label">
             <span className="label-text">A kutya életkora</span>
           </label>
           <input
-            type="text"
+            type="text" value={formData.eletkor} onChange={writeData}
             className="input input-bordered w-full max-w-xs"
             id="eletkor"
           />
@@ -31,7 +56,7 @@ function KutyaForm() {
             <span className="label-text">Utolsó ellenőrzés dátuma</span>
           </label>
           <input
-            type="text"
+            type="text" value={formData.utolsoell} onChange={writeData}
             className="input input-bordered w-full max-w-xs"
             id="utolsoell"
           />
@@ -40,9 +65,9 @@ function KutyaForm() {
           <label className="label">
             <span className="label-text">Válasszon kutyanevet</span>           
           </label>
-          <select className="select select-bordered" id="nev">
+          <select className="select select-bordered" value={formData.nevid} onChange={writeData} id="nevid">
            {
-              kutyanevek.map((kutyanev)=>(<option key={kutyanev.id}>{kutyanev.kutyanev}</option>))
+              kutyanevek.map((kutyanev)=>(<option key={kutyanev.id} value={kutyanev.id}>{kutyanev.kutyanev}</option>))
            } 
           </select>         
         </div>
@@ -50,13 +75,14 @@ function KutyaForm() {
           <label className="label">
             <span className="label-text">Válasszon kutyafajtát!</span>           
           </label>
-          <select className="select select-bordered" id="fajtanev">
+          <select className="select select-bordered" value={formData.fajtaid} onChange={writeData} id="fajtaid">
            {
-             kutyafajtak.map((kutyafajta)=>(<option key={kutyafajta.id}>{kutyafajta.nev}</option>))
+             kutyafajtak.map((kutyafajta)=>(<option key={kutyafajta.id} value={kutyafajta.id}>{kutyafajta.nev}</option>))
            }
           </select>         
         </div>
         <button type="submit" className="btn btn-primary">Küldés</button>
+        </form>
       </div>
     </div>
   );
